1b7793d1e2aff5ef13d2a0e04991fe524185916c
Vinícius <vcljunges@gmail.com>
2026-04-14 14:28:17 -0300

    Finalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@
 
 export default function Home() {
   function handleClick() {
-    alert("EBA!");
+    alert("Evento em function!");
   }
 
   const handleButtonClick = () => {
-    alert("Function em constante");
+    alert("Function em constante!");
   }
 
   const notice = (msg: string) => {
@@ -14,14 +14,14 @@ export default function Home() {
   }
 
   const handleButtonComponent = () => {
-    alert("Evento em componente");
+    alert("Evento em componente!");
   }
 
   return (
     <div className="flex flex-col w-screen justify-center items-center gap-2">
       <button className="p-3 bg-blue-700 text-white rounded-md" onClick={handleClick}>Clique aqui</button>
       <button className="p-3 bg-blue-700 text-white rounded-md" onClick={handleButtonClick}>Clique aqui</button>
-      <button className="p-3 bg-blue-700 text-white rounded-md" onClick={() => notice("SEI LÁ")}>Clique aqui</button>
+      <button className="p-3 bg-blue-700 text-white rounded-md" onClick={() => notice("Function com parâmetro")}>Clique aqui</button>
       <button className="p-3 bg-blue-700 text-white rounded-md" onClick={handleButtonComponent}>Clique aqui</button>
     </div>
   );
